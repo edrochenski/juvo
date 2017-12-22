14f1339ff17eed21d6b79a245c7ee8d411d09fa2
Ed Rochenski <edrochenski@bytedown.com>
2017-12-21 20:48:56 -0500

    added interface for the JuvoClient

---
+++ src/Juvo/IJuvoClient.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IJuvoClient.cs" company="https://gitlab.com/edrochenski/juvo">
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace JuvoProcess
+{
+    using JuvoProcess.Bots;
+
+    /// <summary>
+    /// Representation of a Juvo Client object.
+    /// </summary>
+    public interface IJuvoClient
+    {
+        /// <summary>
+        /// Queues a command for the bot to execute.
+        /// </summary>
+        /// <param name="cmd">Command to execute.</param>
+        void QueueCommand(IBotCommand cmd);
+    }
+}
