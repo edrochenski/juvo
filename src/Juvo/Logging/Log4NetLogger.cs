cef9500238084c9949015ab01d0cfd9ba1fec01c
Ed Rochenski <edrochenski@bytedown.com>
2018-02-14 12:01:33 -0500

    added bridge classes for log4net so we could use it for webhost/kestrel/libuv logging

---
+++ src/Juvo/Logging/Log4NetLogger.cs
@@ -0,0 +1,100 @@
+﻿// <copyright file="Log4NetLogger.cs" company="https://gitlab.com/edrochenski/juvo">
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace JuvoProcess.Logging
+{
+    using System;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// <see cref="ILogger"/>-compatibe log4net logger.
+    /// </summary>
+    public class Log4NetLogger : ILogger
+    {
+        private readonly log4net.ILog log;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log4NetLogger"/> class.
+        /// </summary>
+        /// <param name="log">log4Net log to use.</param>
+        public Log4NetLogger(log4net.ILog log)
+        {
+            this.log = log;
+        }
+
+        /// <inheritdoc/>
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug: return this.log.IsDebugEnabled;
+                case LogLevel.Information: return this.log.IsInfoEnabled;
+                case LogLevel.Warning: return this.log.IsWarnEnabled;
+                case LogLevel.Error: return this.log.IsErrorEnabled;
+                case LogLevel.Critical: return this.log.IsFatalEnabled;
+                default: throw new ArgumentOutOfRangeException(nameof(logLevel));
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception exception,
+            Func<TState, Exception, string> formatter)
+        {
+            if (!this.IsEnabled(logLevel))
+            {
+                return;
+            }
+
+            if (formatter == null)
+            {
+                throw new ArgumentException(nameof(formatter));
+            }
+
+            var message = formatter(state, exception);
+            if (!string.IsNullOrEmpty(message) || exception != null)
+            {
+                this.WriteMessage(logLevel, eventId.Id, message, exception);
+            }
+        }
+
+        private void WriteMessage(LogLevel logLevel, int eventId, string message, Exception exception)
+        {
+            var evtId = eventId == 0 ? string.Empty : $" [{eventId}]";
+
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    this.log.Debug($"{message}{evtId}", exception);
+                    break;
+                case LogLevel.Information:
+                    this.log.Info($"{message}{evtId}", exception);
+                    break;
+                case LogLevel.Warning:
+                    this.log.Warn($"{message}{evtId}", exception);
+                    break;
+                case LogLevel.Error:
+                    this.log.Error($"{message}{evtId}", exception);
+                    break;
+                case LogLevel.Critical:
+                    this.log.Fatal($"{message}{evtId}", exception);
+                    break;
+                default:
+                    this.log.Debug($"{message}{evtId}", exception);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Juvo/Logging/Log4NetLoggerProvider.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="Log4NetLoggerProvider.cs" company="https://gitlab.com/edrochenski/juvo">
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+namespace JuvoProcess.Logging
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// <see cref="ILoggerProvider"/>-compatible provider for log4net.
+    /// </summary>
+    public class Log4NetLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, ILogger> loggers;
+        private string configFileName = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log4NetLoggerProvider"/> class.
+        /// </summary>
+        /// <param name="configFileName">Configuration file name.</param>
+        public Log4NetLoggerProvider(string configFileName)
+        {
+            this.loggers = new ConcurrentDictionary<string, ILogger>();
+            this.configFileName = configFileName;
+        }
+
+        /// <inheritdoc/>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return this.loggers.GetOrAdd(categoryName, this.CreateLoggerImplementation);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.loggers.Clear();
+        }
+
+        private Log4NetLogger CreateLoggerImplementation(string categoryName)
+        {
+            var repository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+            if (log4net.LogManager.GetCurrentLoggers(repository.Name).Count() == 0)
+            {
+                log4net.Config.XmlConfigurator.Configure(repository, new FileInfo(this.configFileName));
+            }
+
+            var logger = log4net.LogManager.GetLogger(repository.Name, categoryName);
+            return new Log4NetLogger(logger);
+        }
+    }
+}
