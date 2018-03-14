f71679f22e42645d42a5941fb0a422e73e6145db
Ed Rochenski <edrochenski@bytedown.com>
2018-03-14 14:38:01 -0400

    added missing juvo/log classes

---
+++ src/Juvo/Juvo/Log/ILogManager.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ILogManager.cs" company="https://gitlab.com/edrochenski/juvo">
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace JuvoProcess
+{
+    using System;
+
+    /// <summary>
+    /// Represents a log manager.
+    /// </summary>
+    public interface ILogManager
+    {
+        /// <summary>
+        /// Gets a logger.
+        /// </summary>
+        /// <param name="type">Type being logged.</param>
+        /// <returns>A logger for the type.</returns>
+        ILog GetLogger(Type type);
+    }
+}
---
+++ src/Juvo/Juvo/Log/LogManagerProxy.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="LogManagerProxy.cs" company="https://gitlab.com/edrochenski/juvo">
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace JuvoProcess
+{
+    using System;
+    using log4net;
+
+    /// <summary>
+    /// Proxy.
+    /// </summary>
+    public class LogManagerProxy : ILogManager
+    {
+        /// <inheritdoc/>
+        public ILog GetLogger(Type type)
+        {
+            return new LogProxy(LogManager.GetLogger(type));
+        }
+    }
+}
