bcaebeb90d90b8c76aea9b0fb89bcc4c7439cd27
Ed Rochenski <edrochenski@bytedown.com>
2018-05-06 11:02:11 -0400

    modified test to include a call to Run()

--- src/Juvo.Tests/Juvo/JuvoClientTests.cs
+++ src/Juvo.Tests/Juvo/JuvoClientTests.cs
@@ -4,11 +4,13 @@
 
 namespace Juvo.Tests.Juvo
 {
+    using System.Collections.Generic;
     using System.IO;
     using System.Threading;
     using JuvoProcess;
     using JuvoProcess.Bots;
     using JuvoProcess.Configuration;
+    using JuvoProcess.IO;
     using Microsoft.AspNetCore.Hosting;
     using Moq;
     using Xunit;
@@ -16,7 +18,7 @@ namespace Juvo.Tests.Juvo
     public class JuvoClientTests
     {
         [Fact]
-        public void CreateInstance()
+        public void CreateInstanceAndRun()
         {
             var instance = new JuvoClient(
                 this.GetWindowsConfiguration(),
@@ -25,19 +27,41 @@ namespace Juvo.Tests.Juvo
                 new Mock<SlackBotFactory>().Object,
                 new Mock<ILogManager>().Object,
                 new Mock<IWebHost>().Object,
+                new Mock<IStorageHandler>().Object,
                 new ManualResetEvent(false));
             Assert.NotNull(instance);
+
+            instance.Run().Wait(1000);
         }
 
         private Config GetWindowsConfiguration()
         {
             return new Config
             {
+                Discord = new DiscordConfig
+                {
+                    Connections = new List<DiscordConfigConnection>(0),
+                    Enabled = false
+                },
+                Irc = new IrcConfig
+                {
+                    Connections = new List<IrcConfigConnection>(0),
+                    Enabled = false
+                },
+                Slack = new SlackConfig
+                {
+                    Connections = new List<SlackConfigConnection>(0),
+                    Enabled = false
+                },
                 System = new SystemInfo
                 {
                     AppDataPath = new DirectoryInfo(@"z:\some\fake\path\app"),
                     LocalAppDataPath = new DirectoryInfo(@"z:\some\fake\path\app\local"),
                     Os = OperatingSystem.Windows
+                },
+                WebServer = new WebServerConfig
+                {
+                    Enabled = false
                 }
             };
         }
